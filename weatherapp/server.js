60612341dae0a6cf605bc163785ce881d2840162
Abhishekmnnit6022 <abhishek.20238030@mnnit.ac.in>
2024-04-11 15:13:32 +0530

    corrected things

---
+++ weatherapp/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('connect-flash');
+const app = express();
+
+app.use(express.static('public'));
+
+// MongoDB Atlas connection string
+const dbURI = 'mongodb+srv://<admin>:<admin123>@cluster0.uxfinir.mongodb.net/myDatabase?retryWrites=true&w=majority';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(flash());
+
+// Routes
+app.use('/', require('../routes/index'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
