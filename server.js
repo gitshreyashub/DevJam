b60a784c016796911b627035add6f575fe1be5a3
Abhishekmnnit6022 <abhishek.20238030@mnnit.ac.in>
2024-04-12 14:42:32 +0530

    Linked home page

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const flash = require('connect-flash');
 const app = express();
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Error connecting to MongoDB', err));
 
